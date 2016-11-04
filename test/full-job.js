f81b3d627a06a85e16df8674f0c6fd550ecc2669
Filip Weiss <me@fiws.net>
2016-11-04 11:27:02 +0100

    better error logging

--- test/full-job.js
+++ test/full-job.js
@@ -43,3 +43,23 @@ test.cb('work with correct options', t => {
     server.bullish.add('testFull1', { a: 5, b: 10 });
   });
 });
+
+test.cb('emit an error if there is one', t => {
+  const server = t.context.server;
+  t.plan(4);
+
+  server.on('bullish failed', (err) => {
+    t.true(err.queueName !== undefined, 'error has queueName');
+    t.true(err.job !== undefined, 'error has job');
+    t.true(err.job.jobId !== undefined, 'job has id');
+    t.end();
+  });
+
+  const sum = () => {
+    throw new Error('I am broken');
+  };
+  server.bullish.job({ name: 'testBrokenFull1', handler: sum }, (e) => {
+    t.true(e === undefined, 'no error');
+    server.bullish.add('testBrokenFull1', { a: 5, b: 10 });
+  });
+});
